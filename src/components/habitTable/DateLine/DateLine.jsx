07aef0082c6a8add94c14c8c7418e91aecf4f77b
am.mussy <aydoganwork@gmail.com>
2022-02-16 17:47:20 +0300

    test 2

--- src/components/habitTable/DateLine/DateLine.jsx
+++ src/components/habitTable/DateLine/DateLine.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import DateСell from "./DateСell/DateСell";
 import s from "./DateLine.module.css";
 
-// >           1|2|3|4|5|6|7|8|9|10asdasd
+// >           1|2|3|4|5|6|7|8|9|10asdasdasdasd
 //  HabitName |X|X|X|X|X|X|X|X|X|X|X|X|X|X|X|X|
 //
 //
